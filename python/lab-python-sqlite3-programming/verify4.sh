d360afb0331eb44141f25696097b2e9d1d0e1601
shuchong-li <lsc13361272744@163.com>
2023-03-06 00:00:51 +0100

    modify verify4.sh

--- python/lab-python-sqlite3-programming/verify4.sh
+++ python/lab-python-sqlite3-programming/verify4.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
-export PYTHONPATH=${PYTHONPATH}:/home/labex/project
 
 output=$(python3 /home/labex/project/sqlite3_programming.py 2>&1)
 # Check if we can query the data in users table
-echo "$output" | grep "Jane Doe"
+#echo "$output" | grep "Jane Doe"
