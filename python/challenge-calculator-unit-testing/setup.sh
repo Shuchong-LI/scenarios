b2729b4b7377950cdc6f08d316387a249dbc3d57
shuchong-li <lsc13361272744@163.com>
2023-03-07 12:23:14 +0100

    review1

--- python/challenge-calculator-unit-testing/setup.sh
+++ python/challenge-calculator-unit-testing/setup.sh
@@ -5,30 +5,3 @@ touch /home/labex/project/calculator_unittest.py
 # Install the unittest package using pip
 pip install unittest
 
-# Add some calculations to calculator_unittest.py
-echo "import unittest
-
-class Calculator:
-    #Performs basic arithmetic operations.
-
-    def add(self, a: float, b: float) -> float:
-        #Return the sum of two numbers.
-        return a + b
-
-    def subtract(self, a: float, b: float) -> float:
-        #Return the difference between two numbers.
-        return a - b
-
-    def multiply(self, a: float, b: float) -> float:
-        #Return the product of two numbers.
-        return a * b
-
-    def divide(self, a: float, b: float) -> float:
-        #Return the quotient of two numbers.
-        #Raises a ValueError if b is zero.
-        if b == 0:
-            raise ValueError(\"Cannot divide by zero\")
-        return a / b
-        
-    
-#TODO: implement unittest for the methods above" > /home/labex/project/calculator_unittest.py
