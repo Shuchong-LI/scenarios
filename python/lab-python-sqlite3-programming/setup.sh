9d6e3e2c7e5718a31dd524daa97864ca58ebd87d
shuchong-li <lsc13361272744@163.com>
2023-03-09 13:31:47 +0100

    fix

--- python/lab-python-sqlite3-programming/setup.sh
+++ python/lab-python-sqlite3-programming/setup.sh
@@ -2,13 +2,3 @@
 
 # Create a new file called sqlite3_programming.py
 touch /home/labex/project/sqlite3_programming.py
-
-# Install sqlite3
-sudo apt-get install sqlite3
-
-# Install the sqlite3 package using pip
-pip install sqlite3
-
-
-unalias grep
-
