d5846d7a225b816344e542ac4a3013f92658b9ba
shuchong-li <lsc13361272744@163.com>
2023-03-17 17:47:14 +0100

    modify verify files

--- linux/challenge-delete-and-move-files/verify2.sh
+++ linux/challenge-delete-and-move-files/verify2.sh
@@ -2,11 +2,4 @@
 
 cat ~/.zsh_history | grep -q "cp mytextfile.txt"
 
-cd /home/labex/Desktop/challenge2
-
-my_file="mytextfile.txt"
-for file in *; do
-    if ! diff -q "$my_file" "$file" > /dev/null; then
-        exit 1
-    fi
-done
+$(ls -l /home/labex/Desktop/challenge2 | wc -l) -ge 2
