47359e00863542a87b2b6f8934622930e0938781
shuchong-li <lsc13361272744@163.com>
2023-03-18 18:38:40 +0100

    change assets

--- linux/challenge-delete-and-move-files/setup.sh
+++ linux/challenge-delete-and-move-files/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
 cd /home/labex/Desktop
-mkdir challenge1 challenge2 challenge3 challenge4
+mkdir challenge1 challenge2 challenge3
+cd challenge1
+touch mytextfile.txt
+echo "I love labex" > mytextfile.txt
