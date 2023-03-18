0b9f87364f0e5d46e44a624825283b570c5417cd
shuchong-li <lsc13361272744@163.com>
2023-03-18 18:28:53 +0100

    setup chmod

--- linux/challenge-delete-and-move-files/setup.sh
+++ linux/challenge-delete-and-move-files/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 cd /home/labex/Desktop
-mkdir challenge1 challenge2 challenge3 
-sudo chown :labex /home/labex/Desktop/challenge1
-sudo chmod g+rw /home/labex/Desktop/challenge1 
+mkdir challenge1 challenge2 challenge3 challenge4
+sudo chown :labex /home/labex/Desktop/challenge4
+sudo chmod g+rw /home/labex/Desktop/challenge4
