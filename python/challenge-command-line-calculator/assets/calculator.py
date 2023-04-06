07a2732ba9916a3a65a1775cadedb7b279f8cb5e
Shuchong-LI <lsc13361272744@163.com>
2023-04-06 13:35:46 +0800

    update

--- python/challenge-command-line-calculator/assets/calculator.py
+++ python/challenge-command-line-calculator/assets/calculator.py
@@ -18,28 +18,8 @@ def divide(a: int, b: int) -> float:
 
 if __name__ == '__main__':
     # TODO: Creat a parser and a group.
-    parser = argparse.ArgumentParser(description='Perform arithmetic operations on two numbers.')
-    group = parser.add_mutually_exclusive_group()
-
+    pass
     # TODO: Add and parse the arguments.
-    group.add_argument('-a', '--add', nargs=2, type=int, metavar=('NUM1', 'NUM2'), help='add NUM1 and NUM2')
-    group.add_argument('-s', '--subtract', nargs=2, type=int, metavar=('NUM1', 'NUM2'), help='subtract NUM2 from NUM1')
-    group.add_argument('-m', '--multiply', nargs=2, type=int, metavar=('NUM1', 'NUM2'), help='multiply NUM1 and NUM2')
-    group.add_argument('-d', '--divide', nargs=2, type=int, metavar=('NUM1', 'NUM2'), help='divide NUM1 by NUM2')
-    args = parser.parse_args()
-
+    pass
     # TODO: Output management.
-    if args.add:
-        result = add(*args.add)
-        print(f"{args.add[0]} + {args.add[1]} = {result}")
-    elif args.subtract:
-        result = subtract(*args.subtract)
-        print(f"{args.subtract[0]} - {args.subtract[1]} = {result}")
-    elif args.multiply:
-        result = multiply(*args.multiply)
-        print(f"{args.multiply[0]} * {args.multiply[1]} = {result}")
-    elif args.divide:
-        result = divide(*args.divide)
-        print(f"{args.divide[0]} / {args.divide[1]} = {result}")
-    else:
-        parser.print_help()
+    pass
