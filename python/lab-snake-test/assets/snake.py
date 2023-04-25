db2dbf606617af22705027234b40ce1902e9ab42
shuchong-li <lsc13361272744@163.com>
2023-04-25 11:57:22 +0800

    test

--- python/lab-snake-test/assets/snake.py
+++ python/lab-snake-test/assets/snake.py
@@ -24,7 +24,7 @@ class SNAKE:
 		self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
 		self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
 		self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
-		self.crunch_sound = pygame.mixer.Sound('Sound/crunch.wav')
+		# self.crunch_sound = pygame.mixer.Sound('Sound/crunch.wav')
 
 	def draw_snake(self):
 		self.update_head_graphics()
@@ -84,8 +84,8 @@ class SNAKE:
 	def add_block(self):
 		self.new_block = True
 
-	def play_crunch_sound(self):
-		self.crunch_sound.play()
+	# def play_crunch_sound(self):
+	# 	self.crunch_sound.play()
 
 	def reset(self):
 		self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
@@ -126,7 +126,7 @@ class MAIN:
 		if self.fruit.pos == self.snake.body[0]:
 			self.fruit.randomize()
 			self.snake.add_block()
-			self.snake.play_crunch_sound()
+			# self.snake.play_crunch_sound()
 
 		for block in self.snake.body[1:]:
 			if block == self.fruit.pos:
